92eff056120c08a4dc106e736de9c47dda4828c1
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-01 09:53:51 -0600

    Part 2

--- about.js
+++ about.js
@@ -5,10 +5,19 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form submitted successfully!');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+function imageHoverCompliment(event) {
+	alert('Nice straight teeth buddy ol\' pal!')
+
+}
+
+let catImg = document.querySelector('#catpic')
+catImg.addEventListener('mouseover', imageHoverCompliment)
